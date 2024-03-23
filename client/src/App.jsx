7e53ceae81bbce5cf132a57e01b8598fcc136bbf
Omramanuj <ramanuj.om.r@gmail.com>
2024-03-23 16:44:45 +0530

    added BMI functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from 'react'
-
 import './App.css'
+import Services from './pages/Services'
+import { Route, Routes } from 'react-router-dom'
+
 
 function App() {
 
   return (
     <>
-      
+    <Routes>
+      <Route path="/services" element={<Services/>}></Route>
+    </Routes>
+
     </>
   )
 }
---
+++ client/src/pages/Services.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import BMI from '../components/BMI'
+
+export default function Services() {
+  return (
+    <>
+    <div className="akhu flex flex-col">
+        <div className="services "></div>
+        <div className="bmi "><BMI/></div>
+    </div>
+
+
+    </>
+  )
+}
